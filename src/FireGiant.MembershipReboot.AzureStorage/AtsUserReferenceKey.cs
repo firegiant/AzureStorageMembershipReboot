de87c33a8f9d0998748a94989228268a33068c0e
Rob Mensching <rob@firegiant.com>
2016-05-21 01:04:04 -0700

    Use better separator in user reference keys
    Equals is a great separator since a reference key really is a collection of name value pairs. So use that with the veritcal bar separating pairs.

--- src/FireGiant.MembershipReboot.AzureStorage/AtsUserReferenceKey.cs
+++ src/FireGiant.MembershipReboot.AzureStorage/AtsUserReferenceKey.cs
@@ -14,7 +14,7 @@ namespace FireGiant.MembershipReboot.AzureStorage
 
         private AtsUserReferenceKey(string tenant, string referenceKey)
         {
-            this.Partition = "tenant|" + tenant + "|" + referenceKey;
+            this.Partition = "tenant=" + tenant + "|" + referenceKey;
             this.Row = String.Empty;
         }
 
@@ -24,32 +24,32 @@ namespace FireGiant.MembershipReboot.AzureStorage
 
         public static AtsUserReferenceKey ForUsername(string tenant, string username)
         {
-            return new AtsUserReferenceKey(tenant, "user|" + username.ToBase64IfUnsafe());
+            return new AtsUserReferenceKey(tenant, "user=" + username.ToBase64IfUnsafe());
         }
 
         public static AtsUserReferenceKey ForEmail(string tenant, string email)
         {
-            return new AtsUserReferenceKey(tenant, "email|" + email.ToBase64IfUnsafe());
+            return new AtsUserReferenceKey(tenant, "email=" + email.ToBase64IfUnsafe());
         }
 
         public static AtsUserReferenceKey ForPhoneNumber(string tenant, string phone)
         {
-            return new AtsUserReferenceKey(tenant, "phone|" + phone);
+            return new AtsUserReferenceKey(tenant, "phone=" + phone);
         }
 
         public static AtsUserReferenceKey ForVerificationKey(string key)
         {
-            return new AtsUserReferenceKey("key|" + key);
+            return new AtsUserReferenceKey("key=" + key);
         }
 
         public static AtsUserReferenceKey ForLinkedAccount(string tenant, string provider, string id)
         {
-            return new AtsUserReferenceKey(tenant, "link|" + provider + "|" + id.ToBase64IfUnsafe());
+            return new AtsUserReferenceKey(tenant, "link=" + provider + "|" + id.ToBase64IfUnsafe());
         }
 
         public static AtsUserReferenceKey ForCertificate(string tenant, string thumbprint)
         {
-            return new AtsUserReferenceKey(tenant, "cert|" + thumbprint);
+            return new AtsUserReferenceKey(tenant, "cert=" + thumbprint);
         }
     }
 }
